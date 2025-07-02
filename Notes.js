0a1d7951a7d7426991987d23b2ab21ccd4fb27c7
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-02 21:36:52 +0530

    update

---
+++ Notes.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelector("button");
+const Noteshows = document.getElementsByClassName(".noteshow");
+const inputboxs = document.getElementsByClassName(".input-box");
+
+buttons.addEventListener("click", (button) => {
+    
+});
