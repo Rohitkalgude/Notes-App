f54f047dde04da2f2f688c2d495990e9e00fd532
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-03 10:45:24 +0530

    update

--- Notes.js
+++ Notes.js
@@ -1,7 +1,21 @@
-const buttons = document.querySelector("button");
-const Noteshows = document.getElementsByClassName(".noteshow");
-const inputboxs = document.getElementsByClassName(".input-box");
+const createbtn = document.querySelector("button");
+const noteshow = document.querySelector(".noteshow");
 
-buttons.addEventListener("click", (button) => {
-    
+createbtn.addEventListener("click", () => {
+  const newnote = document.createElement("p");
+  newnote.className = "input-box";
+  newnote.contentEditable = "true";
+  newnote.innerHTML = `<img src="./image/delete.png" alt="delete" class="delete">`;
+
+  noteshow.appendChild(newnote);
+  newnote.focus();
 });
+
+
+const deleteBtn = newnote.querySelector(".delete");
+
+deleteBtn.addEventListener("click", () => {
+  newnote.remove();
+});
+
+
